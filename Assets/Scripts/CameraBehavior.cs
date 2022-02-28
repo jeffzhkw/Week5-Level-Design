756d5983c4209d1b9a3fab23dd6bec927b31c4cb
Kewei Zhang <jeffzhkw@gmail.com>
2022-02-28 14:44:01 -0500

    Added CameraBehavior, incomplete PlayerController

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject playerObj;
+    //public float minX, maxX; //set after level design
+    //public float minY, maxY;
+
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - playerObj.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desirePos = playerObj.transform.position + offset;
+        //desirePos = new Vector3(
+        //    Mathf.Clamp(desirePos.x, minX, maxX),
+        //    Mathf.Clamp(desirePos.y, minY, maxY),
+        //    desirePos.z);
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desirePos, 0.125f);
+        transform.position = smoothPos;
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public float speed;
+    public float jumpForce;
+
+    private Vector2 movement;
+    private Rigidbody2D playerRb;
+    private bool isGrounded;
+
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (isGrounded)
+            {
+                playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isGrounded = false;
+            }
+        }
+        //playerRb.velocity = new Vector2(movement.x * speed, playerRb.velocity.y);
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check for ground collision.
+        isGrounded = true;
     }
 }
